355f706bb158ce7c9f590d29c2cbf960103de656
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2023-07-12 20:53:31 -0300

    feat: create game entity class

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -1,2 +1,26 @@
-package com.devsuperior.dslist.entities;public class Game {
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "games")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Column(name = "game_year")
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imageUrl;
+    private String shortDescription;
+    private String longDescription;
 }
