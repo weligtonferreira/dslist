8f40b193ebaedb801b8e8aa9af43510e78dd80c8
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2023-07-13 13:02:11 -0300

    feat: create belonging pk entity

---
+++ src/main/java/com/devsuperior/dslist/entities/BelongingPk.java
@@ -0,0 +1,22 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class BelongingPk {
+    @ManyToOne
+    @JoinColumn(name = "game_id")
+    private Game game;
+
+    @ManyToOne
+    @JoinColumn(name = "list_id")
+    private GameList gameList;
+}
