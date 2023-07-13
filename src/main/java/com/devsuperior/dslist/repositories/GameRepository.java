62fa85d9622878d403141023082482575205b8aa
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2023-07-13 10:56:28 -0300

    feat: add game repository

---
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -0,0 +1,6 @@
+package com.devsuperior.dslist.repositories;
+
+import com.devsuperior.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {}
