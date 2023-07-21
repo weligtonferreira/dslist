c79f3669df9e400175212c7b00a4dfee44d0e27e
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2023-07-21 16:59:27 -0300

    feat: add constructor for game min projection

--- src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dslist.dto;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
@@ -17,6 +18,14 @@ public class GameMinDTO {
         year = entity.getYear();
         imageUrl = entity.getImageUrl();
         shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imageUrl = projection.getImageUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
