679b487620e28aa2c2510fcb432b616aff46f719
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2023-07-21 16:57:22 -0300

    feat: add custom sql query

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -1,6 +1,20 @@
 package com.devsuperior.dslist.repositories;
 
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface GameRepository extends JpaRepository<Game, Long> {}
+import java.util.List;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+    @Query(nativeQuery = true, value = """
+            SELECT games.id, games.title, games.game_year AS `year`, games.image_url AS imageUrl,
+            games.short_description AS shortDescription, belonging.position
+            FROM games
+            INNER JOIN belonging ON games.id = belonging.game_id
+            WHERE belonging.list_id = :listId
+            ORDER BY belonging.position
+            """)
+    List<GameMinProjection> searchByList(Long listId);
+}
