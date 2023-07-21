21eb39cfb8a41fb468f97aff9a5f403603413745
Weligton Ferreira <57678361+weligtonferreira@users.noreply.github.com>
2023-07-21 16:55:29 -0300

    feat: create game min projection interface

---
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dslist.projections;
+
+public interface GameMinProjection {
+    Long getId();
+    String getTitle();
+    Integer getYear();
+    String getImageUrl();
+    String getShortDescription();
+    Integer getPosition();
+}
